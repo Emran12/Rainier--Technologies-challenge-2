4dbcd855e501032c050976088f550dba2609dc45
Emran12 <emran6452@gmail.com>
2022-06-22 21:36:16 +0600

    added banner

--- src/Components/Appointments/Appointments.js
+++ src/Components/Appointments/Appointments.js
@@ -1,29 +1,11 @@
-import React from "react";
+import React from 'react';
 
 const Appointments = () => {
-  return (
-    <div>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ipsum
-        in deserunt nulla facere laudantium, est architecto unde numquam quam
-        sit saepe harum quas aperiam sunt minus. Sint distinctio impedit,
-        dolores rem rerum odio accusamus. Voluptatum quod, ratione ut ullam
-        facere animi accusamus sapiente nostrum soluta veritatis, ad incidunt.
-        Placeat tempore debitis magni totam quisquam? Consequuntur eveniet magni
-        sapiente commodi nulla harum dicta optio sunt, libero ipsam unde, porro
-        enim ipsa laboriosam inventore mollitia impedit reiciendis dolorum odit.
-        Dolorum, dolore. A, adipisci. Ut esse cupiditate vel quia eligendi
-        libero doloribus, impedit ipsum error excepturi perferendis iusto eum
-        quisquam, facere tenetur id amet dolorem. Deleniti tempora nisi
-        repellat, dolorum, quam perspiciatis ipsam pariatur ex odio dolores est
-        molestiae porro expedita hic animi quasi modi asperiores. Deserunt hic
-        assumenda, dolor exercitationem id aspernatur molestias repellat
-        recusandae veniam impedit, iure tempora similique dolorem ipsum
-        voluptatibus delectus incidunt sapiente sunt quae aut. Consequuntur,
-        magnam?
-      </p>
-    </div>
-  );
+    return (
+        <div>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime saepe nostrum enim dolorum consectetur aspernatur aliquid libero officia, ipsam veritatis doloremque dolor et voluptas totam excepturi rerum? Necessitatibus nisi placeat et quo impedit aliquam odit voluptatibus veniam reiciendis velit quis exercitationem corrupti quibusdam, sapiente eaque tenetur, unde esse beatae praesentium delectus. Suscipit veritatis, ipsa repellendus nesciunt ipsam totam repellat, amet delectus molestiae voluptate consectetur sint rerum, ratione minus culpa eveniet! Quis adipisci aspernatur doloremque modi dolor reprehenderit amet esse commodi, quasi fugit exercitationem nobis recusandae ex maxime similique! Nemo quidem quam quos quas neque magni modi, suscipit hic deserunt, doloribus facere delectus, accusantium omnis culpa mollitia fugit praesentium commodi. Animi repellendus aspernatur cupiditate iste quam accusamus. Saepe quis quo in facere nam maxime officiis impedit earum aspernatur, accusamus esse deserunt itaque iure consequatur perspiciatis et at deleniti ipsa. Possimus porro omnis obcaecati eaque odit sint tenetur dolore enim aspernatur eos.</p>
+        </div>
+    );
 };
 
 export default Appointments;
--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,27 +1,18 @@
 import React from "react";
+import "./Banner.css";
+import img from "../../Assests/images/Svg_sample1.png";
 
 const Banner = () => {
   return (
-    <div>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus ab
-        eius optio modi repudiandae! Quaerat a laboriosam veniam dicta,
-        assumenda labore blanditiis corporis numquam placeat tempora dolorum ut.
-        Modi nesciunt aspernatur illum quis ducimus repudiandae nihil ipsa
-        laborum cumque mollitia distinctio incidunt est ut perspiciatis
-        excepturi, totam rerum odit voluptas reiciendis numquam quisquam sit,
-        facilis asperiores sint. Dolorem eaque perferendis debitis ut alias
-        ducimus animi, similique quibusdam? Repellat molestias alias sit fugit
-        doloribus ab iure, nemo necessitatibus est labore veritatis. Blanditiis
-        nisi reprehenderit excepturi fugit ut nesciunt voluptas optio nostrum
-        molestias earum dolores quisquam debitis fuga aliquam nemo, labore quae
-        sequi quibusdam aspernatur aut, quaerat amet. Eaque inventore ex at
-        explicabo ab quaerat doloribus officiis exercitationem itaque tempore
-        minima natus enim, unde eligendi possimus ipsam consectetur rem culpa
-        fugit? Illo necessitatibus rerum animi. Nam debitis corrupti sapiente,
-        ipsam ducimus distinctio ipsa, aliquam, ullam dolor ea assumenda dicta
-        nemo atque. Error.
-      </p>
+    <div className="banner">
+      <img src={img} alt="" />
+      <div className="text-white flex flex-col justify-center items-center font-['Poppins'] pr-[22px]">
+        <h1 className="text-[24px] leading-[36px]">Hello, Mary Jane!</h1>
+        <p className="text-[16px] leading-[150%]">
+          Stay Up-to-Date with your appointments.
+        </p>
+        <p>You Have No pending Reports</p>
+      </div>
     </div>
   );
 };
